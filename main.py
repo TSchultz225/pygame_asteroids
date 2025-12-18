27a2722a986dddebe1fbcaa2d378c626de2dfd8f
TSchultz225 <tschultz225@gmail.com>
2025-12-18 08:47:19 -0500

    add player Class, draw functionality, player_radius and line_width constants

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from constants import SCREEN_WIDTH
 from constants import SCREEN_HEIGHT
 from logger import log_state
+import player
 
 def main():
     pygame.init()
@@ -13,12 +14,15 @@ def main():
     game_clock = pygame.time.Clock()
     dt = 0
 
+    game_player = player.Player(SCREEN_WIDTH /2,SCREEN_HEIGHT /2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         log_state()
         screen.fill("black")
+        game_player.draw(screen)
         pygame.display.flip()
         dt = game_clock.tick(60) / 1000
 
