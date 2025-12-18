76e97ab7bc6c292ba145c8377a9c9d25824e048d
TSchultz225 <tschultz225@gmail.com>
2025-12-18 12:14:02 -0500

    add comments, update explosion particle with constant

--- asteroid.py
+++ asteroid.py
@@ -18,38 +18,49 @@ class Asteroid(circleshape.CircleShape):
     def split(self):
         self.kill()
 
+        #check if asteroid should "explode" - happens when asteroid is the smallest asteroid possible
         if self.radius <= constants.ASTEROID_MIN_RADIUS:
-            
+            #create angles for explosion particles
+            #number of angles = particles / 2
             explo_particle1_random_angle = random.uniform(5,55)
             explo_particle2_random_angle = random.uniform(65,115)
             explo_particle3_random_angle = random.uniform(125,175)
 
+            #particle 1
             explo_particle1_new_rotation = self.velocity.rotate(explo_particle1_random_angle)
             explo_particle1 = explosionparticle.ExplosionParticle(self.position.x, self.position.y)
-            explo_particle1.velocity = explo_particle1_new_rotation * 0.8
+            explo_particle1.velocity = explo_particle1_new_rotation * constants.EXPLOSION_PARTICLE_VELOCITY_MOD
             
+            #particle 2
             explo_particle2_new_rotation = self.velocity.rotate(explo_particle2_random_angle)
             explo_particle2 = explosionparticle.ExplosionParticle(self.position.x, self.position.y)
-            explo_particle2.velocity = explo_particle2_new_rotation * 0.8
+            explo_particle2.velocity = explo_particle2_new_rotation * constants.EXPLOSION_PARTICLE_VELOCITY_MOD
             
+            #particle 3
             explo_particle3_new_rotation = self.velocity.rotate(explo_particle3_random_angle)
             explo_particle3 = explosionparticle.ExplosionParticle(self.position.x, self.position.y)
-            explo_particle3.velocity = explo_particle3_new_rotation * 0.8
+            explo_particle3.velocity = explo_particle3_new_rotation * constants.EXPLOSION_PARTICLE_VELOCITY_MOD
 
+            #particle 4
             explo_particle4_new_rotation = self.velocity.rotate(-explo_particle1_random_angle)
             explo_particle4 = explosionparticle.ExplosionParticle(self.position.x, self.position.y)
-            explo_particle4.velocity = explo_particle4_new_rotation * 0.8
+            explo_particle4.velocity = explo_particle4_new_rotation * constants.EXPLOSION_PARTICLE_VELOCITY_MOD
             
+            #particle 5
             explo_particle5_new_rotation = self.velocity.rotate(-explo_particle2_random_angle)
             explo_particle5 = explosionparticle.ExplosionParticle(self.position.x, self.position.y)
-            explo_particle5.velocity = explo_particle5_new_rotation * 0.8
+            explo_particle5.velocity = explo_particle5_new_rotation * constants.EXPLOSION_PARTICLE_VELOCITY_MOD
             
+            #particle 6
             explo_particle6_new_rotation = self.velocity.rotate(-explo_particle3_random_angle)
             explo_particle6 = explosionparticle.ExplosionParticle(self.position.x, self.position.y)
-            explo_particle6.velocity = explo_particle6_new_rotation * 0.8
+            explo_particle6.velocity = explo_particle6_new_rotation * constants.EXPLOSION_PARTICLE_VELOCITY_MOD
             return
 
+        #log event
         log_event("asteroid_split")
+
+        #create new split asteroids
         random_angle = random.uniform(20,50)
         new_rotation_asteroid1 = self.velocity.rotate(random_angle)
         new_rotation_asteroid2 = self.velocity.rotate(-random_angle)
--- main.py
+++ main.py
@@ -14,75 +14,112 @@ from logger import log_state
 from logger import log_event
 
 def main():
+    #initialize pygame
     pygame.init()
+    
+    #create font for score display
     my_font = pygame.font.SysFont('Comic Sans MS', 30)
+
+    #create game screen for display
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    #debug info
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    #score tracking info
     score = 0
     asteroids_shot = 0
 
+    #create game clock and delta time vars
     game_clock = pygame.time.Clock()
     dt = 0
 
+    #sprite groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
     shots = pygame.sprite.Group()
     explosion_particles = pygame.sprite.Group()
 
+    #class sprite group additions
     player.Player.containers = (updatable, drawable)
     asteroid.Asteroid.containers = (asteroids, updatable, drawable)
     asteroidfield.AsteroidField.containers = (updatable)
     shot.Shot.containers = (shots, updatable, drawable)
     explosionparticle.ExplosionParticle.containers = (explosion_particles,drawable,updatable)
 
+    #create player instance
     game_player = player.Player(SCREEN_WIDTH /2,SCREEN_HEIGHT /2)
 
+    #create asteroid field instance
     asteroid_field = asteroidfield.AsteroidField()
+
+    #define player as not dead - will allow to break game loop
     playerDead = False
+
     #GAME LOOP
     while playerDead!= True:
+        
+        #Allows the exit button to function
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        #log game state to log file                
         log_state()
+
+        #fill the screen black
         screen.fill("black")
+        
+        # Sets the delta time to update all updatable objects with
         updatable.update(dt)
-        text_surface = my_font.render(f"Score: {math.floor(score)}", False, (180, 180, 180))
+
+        #check if player collides with asteroid
         for asteroid_obj in asteroids:
             if asteroid_obj.collides_with(game_player):
                 log_event("player_hit")
-                print("Game over!")
-                print(f"{score}")
                 playerDead = True
-                
-                
+
+        #update player score                
         score += dt
 
+        #check if shots collide with asteroids
         for asteroid_obj in asteroids:
             for shot_obj in shots:
                 if asteroid_obj.collides_with(shot_obj):
                     log_event("asteroid_shot")
                     asteroid_obj.split()
                     shot_obj.kill()
+
+                    #update score with asteroid point value
+                    #TODO: Update with formal score class
                     score += ASTEROID_POINT_VALUE
                     asteroids_shot += 1
 
+        #check for explosion particle expiration
         for explo_part_obj in explosion_particles:
             if explo_part_obj.current_life_length >= EXPLOSION_PARTICLE_LIFE:
                 explo_part_obj.kill()
 
+        #draw all drawable objects
         for draw_able in drawable:
             draw_able.draw(screen)
+
+        #create text_surface to display score
+        text_surface = my_font.render(f"Score: {math.floor(score)}", False, (180, 180, 180))
+
+        #display score board
         if playerDead!=True:
             screen.blit(text_surface, (SCREEN_WIDTH / 2.4 , 0))
+
+        #refresh game display
         pygame.display.flip()
         
+        #update delta time
         dt = game_clock.tick (60) / 1000
     
+    #scoreboard
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
