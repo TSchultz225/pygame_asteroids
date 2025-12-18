361b712577b497f13ac2682863c0570df808b79b
TSchultz225 <tschultz225@gmail.com>
2025-12-18 09:27:07 -0500

    add asteroid and asteroidfield classes, overwrite draw methods for asteroids, add new constants

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from constants import SCREEN_WIDTH
 from constants import SCREEN_HEIGHT
 from logger import log_state
 import player
+import asteroid
+import asteroidfield
 
 def main():
     pygame.init()
@@ -14,16 +16,26 @@ def main():
     game_clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids_group = pygame.sprite.Group()
+    player.Player.containers = (updatable, drawable)
+    asteroid.Asteroid.containers = (asteroids_group, updatable, drawable)
+    asteroidfield.AsteroidField.containers = (updatable)
     game_player = player.Player(SCREEN_WIDTH /2,SCREEN_HEIGHT /2)
 
+    asteroid_field = asteroidfield.AsteroidField()
+
+    #GAME LOOP
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         log_state()
         screen.fill("black")
-        game_player.draw(screen)
-        game_player.update(dt)
+        updatable.update(dt)
+        for draw_able in drawable:
+            draw_able.draw(screen)
 
         pygame.display.flip()
         dt = game_clock.tick(60) / 1000
