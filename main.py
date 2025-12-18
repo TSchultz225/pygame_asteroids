4585c8946b1e7da2e48d0b07f8cc51e59720f4c6
TSchultz225 <tschultz225@gmail.com>
2025-12-18 08:28:27 -0500

    add circleshape basics, update main.py with game clock

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    game_clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -16,6 +20,7 @@ def main():
         log_state()
         screen.fill("black")
         pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
